9f6b60a2e7bce955345ec08dbce2ac15139674ca
boxanm <matej.boxan@gmail.com>
2024-12-10 09:49:23 -0500

    wip

--- build_system/ubuntu/lpm_install_dependencies_general_ubuntu.bash
+++ build_system/ubuntu/lpm_install_dependencies_general_ubuntu.bash
@@ -105,7 +105,7 @@ n2st::teamcity_service_msg_blockClosed
 # .................................................................................................
 
 n2st::teamcity_service_msg_blockOpened "Install CMake with pip"
-pip install cmake
+python3 -m pip install cmake --verbose
 print_msg "Cmake version is $(cmake --version)"
 n2st::teamcity_service_msg_blockClosed
 # .................................................................................................
